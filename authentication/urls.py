98dcc4c8b43c55a9ce2282be54dab35d8fc353a6
Poriya <pouryachary.8228@gmail.com>
2024-08-17 12:32:55 +0330

    create register user

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,10 +3,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from . import views
 
 urlpatterns = [
     
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.CreateUser.as_view(), name="register_user"),
     
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from authentication.serializers import CreateUserProfileSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class CreateUser(APIView):
+    def post(self, request):
+        req_data = request.data
+        serilizer = CreateUserProfileSerializer(data=req_data)
+        serilizer.is_valid(raise_exception=True)
+        data = serilizer.validated_data
+        user_data = User(
+            username =data["username"],
+            email=data["email"]
+        
+        )
+        
+        user_data.set_password(data["password"])
+        
+        user_data.save()
+        
+        return Response(serilizer.data)
+
+
+
+
+
+
